f1ed2250e484af7f8ee5288c349e024551969381
Carlos Camacho <carloscamacho.ggl.accnt01@gmail.com>
2021-06-03 16:03:44 -0500

    proyecto vacio

---
+++ src/main/java/com/banco/main/ProyectoBancario01Application.java
@@ -0,0 +1,13 @@
+package com.banco.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoBancario01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoBancario01Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/banco/main/ProyectoBancario01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.banco.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoBancario01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
