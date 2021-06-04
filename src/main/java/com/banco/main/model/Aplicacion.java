dba0b31c138a2b4e4a200d0c98def821961bf2a3
Carlos Camacho <carloscamacho.ggl.accnt01@gmail.com>
2021-06-04 13:36:16 -0500

    Relaciones para Aplicación

--- src/main/java/com/banco/main/model/Aplicacion.java
+++ src/main/java/com/banco/main/model/Aplicacion.java
@@ -2,9 +2,12 @@ package com.banco.main.model;
 
 import java.util.Date;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 @Entity
@@ -16,6 +19,13 @@ public class Aplicacion {
 	private int folio;
 	
 	//reserved for one to one relationship
+	@OneToOne
+	@JoinColumn(name = "id_usuario")
+	private Usuario usuario;
+	
+	@OneToOne
+	@JoinColumn(name = "id_producto")
+	private Producto producto;
 	
 	@Column(name = "fecha_aplicacion")
 	private Date fechaAplicacion;
@@ -32,8 +42,10 @@ public class Aplicacion {
 	public Aplicacion() {
 	}
 	
-	public Aplicacion(int folio, Date fechaAplicacion, char aprobacion, Date fechaAprovacion, int montoAutorizado) {
+	public Aplicacion(int folio, Usuario usuario, Producto producto, Date fechaAplicacion, char aprobacion, Date fechaAprovacion, int montoAutorizado) {
 		this.folio = folio;
+		this.usuario = usuario;
+		this.producto = producto;
 		this.fechaAplicacion = fechaAplicacion;
 		this.aprobacion = aprobacion;
 		this.fechaAprovacion = fechaAprovacion;
@@ -80,9 +92,28 @@ public class Aplicacion {
 		this.montoAutorizado = montoAutorizado;
 	}
 	
+	//getter & setters for each relationship
+	
+	public Usuario getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
+	}
+
+	public Producto getProducto() {
+		return producto;
+	}
+
+	public void setProducto(Producto producto) {
+		this.producto = producto;
+	}
+
+	//I've changed the method in order to print also a object of user and an object of product
 	@Override
 	public String toString() {
-		return "Aplicacion [folio=" + folio + ", fechaAplicacion=" + fechaAplicacion + ", aprobacion=" + aprobacion
+		return "Aplicacion [folio=" + folio + ", Usuario="+ usuario +", Producto=" + producto + ", fechaAplicacion=" + fechaAplicacion + ", aprobacion=" + aprobacion
 				+ ", fechaAprovacion=" + fechaAprovacion + ", montoAutorizado=" + montoAutorizado + "]";
 	}
 }
